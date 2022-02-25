edd230e97fe47cfaf142288b3fca24ee38afe13b
Xazeq <setezey@gmail.com>
2022-02-25 09:27:56 +0300

    0. Spring Liquibase [#302330]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -30,12 +30,3 @@ CREATE TABLE IF NOT EXISTS posts_comments(
     post_id    INT REFERENCES posts(id),
     comment_id INT REFERENCES comments(id)
 );
-
-INSERT INTO authorities (authority)
-VALUES ('ROLE_USER');
-INSERT INTO authorities (authority)
-VALUES ('ROLE_ADMIN');
-
-INSERT INTO users (username, enabled, password, authority_id)
-VALUES ('root', true, '$2a$10$8KtefkPSqiodMhZVh9myfeDaEQJ5IgfELfvFn4zMlxC5yjnM9Gi2W',
-        (SELECT id FROM authorities where authority = 'ROLE_ADMIN'));
